bbe2769298bd2a3299ca380aeaf59a8387472411
gabi emi <gabiemi2021@gmail.com>
2025-09-22 16:03:00 -0300

    Criação rota delete

--- src/controllers/trilhasSonorasController.js
+++ src/controllers/trilhasSonorasController.js
@@ -1,6 +1,19 @@
 import dados from "../models/dados.js";
 const { trilhasSonoras } = dados;
 
+const validarTrilha = (trilha) => {
+    const duracaoCadaFaixa = parseInt(trilha.duracao)
+    const duracaoTotal = trilha.faixas * duracaoCadaFaixa;
+
+    if (duracaoTotal) {
+      return "Duração total deve corresponder à soma das faixas individuais";
+    }
+    if (trilha.faixas < 0) {
+      return "Número de faixas deve ser maior que 0";
+    }
+    return null;
+  };
+
 const getAllTrilhas = (req, res) => {
     const { midia, compositor, genero, plataforma } = req.query;
     let resultado = trilhasSonoras;
@@ -26,7 +39,7 @@ const getAllTrilhas = (req, res) => {
 
     res.status(200).json({
         total: resultado.length,
-        obras: resultado,
+        trilhas: resultado,
     });
 };
 
@@ -47,4 +60,57 @@ const getTrilhaByld = (req, res) => {
     })
 };
 
-export { getAllTrilhas, getTrilhaByld };
+const createTrilha = (req, res) => {
+    const { titulo, compositor, midia, genero, faixas, duracao,anoLancamento, plataforma } = req.body;
+
+    const novaTrilha = {
+        id: trilhasSonoras.length + 1,
+        titulo,
+        compositor,
+        midia,
+        genero,
+        faixas: parseInt(faixas),
+        duracao,
+        anoLancamento: parseInt(anoLancamento),
+        plataforma
+    }
+
+    trilhasSonoras.push(novaTrilha);
+
+    res.status(201).json({
+        success: true,
+        message: "Nova trilha cadastrada com sucesso!",
+        data: novaTrilha
+    });
+};
+
+const deleteTrilha = (req, res) => {
+    const id = parseInt(req.params.id);
+
+    if (isNaN(id)) {
+        return res.status(404).json({
+            success: false,
+            message: "O id deve ser válido!"
+        });
+    }
+
+    const trilhaParaRemover = trilhasSonoras.find(t => t.id === id);
+
+    if (!trilhaParaRemover) {
+        return res.status(404).json({
+            success: false,
+            message: `Trilha com o id ${id} não existe!`
+        });
+    }
+
+    const trilhasFiltradas = trilhasSonoras.filter(trilha => trilha.id !== id);
+
+    trilhasSonoras.splice(0, trilhasSonoras.length, ...trilhasFiltradas);
+
+    res.status(200).json({
+        success: true,
+        message: `A trilha com o id ${id} foi removida`
+    });
+};
+
+export { getAllTrilhas, getTrilhaByld, createTrilha, deleteTrilha, updateTrilha };
--- src/routes/trilhasSonorasRoutes.js
+++ src/routes/trilhasSonorasRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { getAllTrilhas, getTrilhaByld } from "../controllers/trilhasSonorasController.js";
+import { getAllTrilhas, getTrilhaByld, createTrilha, deleteTrilha } from "../controllers/trilhasSonorasController.js";
 
 const router = express.Router();
 
 router.get("/", getAllTrilhas);
 router.get("/:id", getTrilhaByld);
+router.post("/", createTrilha);
+router.delete("/:id", deleteTrilha);
 
 export default router;

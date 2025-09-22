8d38306f5766e1fa136cc66dc4845ca5354b278a
gabi emi <gabiemi2021@gmail.com>
2025-09-22 13:48:49 -0300

    Criação dos arquivos e dados

---
+++ server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import dotenv from "dotenv";
+import trilhasSonorasRoutes from "./src/routes/trilhasSonorasRoutes.js";
+
+const app = express();
+app.use(express.json());
+
+dotenv.config();
+const serverPort = process.env.PORT || 3001;
+
+app.get("/", (req, res) => {
+  res.send("🚀 Servidor funcionando");
+});
+
+// 👉 Rotas
+app.use("/trilhasSonoras", trilhasSonorasRoutes);
+
+app.listen(serverPort, () =>
+  console.log(`Servidor rodando na porta ${serverPort} ➝ http://localhost:${serverPort}`)
+);
---
+++ src/models/dados.js
@@ -0,0 +1,114 @@
+const trilhasSonoras = [
+    {
+        "id": "1",
+        "titulo": "The Legend of Zelda: Ocarina of Time",
+        "compositor": "Koji Kondo",
+        "midia": "Videogame",
+        "genero": "Fantasia",
+        "faixas": 82,
+        "duracao": "1h 50min",
+        "anoLancamento": 1998,
+        "plataforma": "Nintendo 64"
+    },
+    {
+        "id": "2",
+        "titulo": "Interstellar",
+        "compositor": "Hans Zimmer",
+        "midia": "Filme",
+        "genero": "Ficção Científica",
+        "faixas": 16,
+        "duracao": "1h 17min",
+        "anoLancamento": 2014,
+        "plataforma": "Cinema"
+    },
+    {
+        "id": "3",
+        "titulo": "Game of Thrones",
+        "compositor": "Ramin Djawadi",
+        "midia": "Série de TV",
+        "genero": "Drama Épico",
+        "faixas": 35,
+        "duracao": "1h 15min",
+        "anoLancamento": 2011,
+        "plataforma": "HBO"
+    },
+    {
+        "id": "4",
+        "titulo": "The Last of Us Part II",
+        "compositor": "Gustavo Santaolalla, Mac Quayle",
+        "midia": "Videogame",
+        "genero": "Drama Pós-Apocalíptico",
+        "faixas": 40,
+        "duracao": "1h 37min",
+        "anoLancamento": 2020,
+        "plataforma": "PlayStation 4"
+    },
+    {
+        "id": "5",
+        "titulo": "A Viagem de Chihiro",
+        "compositor": "Joe Hisaishi",
+        "midia": "Animação",
+        "genero": "Fantasia, Aventura",
+        "faixas": 21,
+        "duracao": "1h 01min",
+        "anoLancamento": 2001,
+        "plataforma": "Cinema"
+    },
+    {
+        "id": "6",
+        "titulo": "Blade Runner 2049",
+        "compositor": "Hans Zimmer, Benjamin Wallfisch",
+        "midia": "Filme",
+        "genero": "Ficção Científica, Ambient",
+        "faixas": 24,
+        "duracao": "1h 45min",
+        "anoLancamento": 2017,
+        "plataforma": "Cinema"
+    },
+    {
+        "id": "7",
+        "titulo": "Cyberpunk 2077",
+        "compositor": "Marcin Przybyłowicz, P.T. Adamczyk, Paul Leonard-Morgan",
+        "midia": "Videogame",
+        "genero": "Synthwave, Eletrônica",
+        "faixas": 50,
+        "duracao": "2h 30min",
+        "anoLancamento": 2020,
+        "plataforma": "PC, PlayStation 4, Xbox One"
+    },
+    {
+        "id": "8",
+        "titulo": "Guardiões da Galáxia",
+        "compositor": "Tyler Bates",
+        "midia": "Filme",
+        "genero": "Rock, Pop",
+        "faixas": 12,
+        "duracao": "44min",
+        "anoLancamento": 2014,
+        "plataforma": "Cinema"
+    },
+    {
+        "id": "9",
+        "titulo": "Stranger Things",
+        "compositor": "Kyle Dixon, Michael Stein",
+        "midia": "Série de TV",
+        "genero": "Synthwave, Horror",
+        "faixas": 36,
+        "duracao": "1h 26min",
+        "anoLancamento": 2016,
+        "plataforma": "Netflix"
+    },
+    {
+        "id": "10",
+        "titulo": "O Senhor dos Anéis: A Sociedade do Anel",
+        "compositor": "Howard Shore",
+        "midia": "Filme",
+        "genero": "Fantasia, Épico",
+        "faixas": 18,
+        "duracao": "1h 11min",
+        "anoLancamento": 2001,
+        "plataforma": "Cinema"
+    }
+]
+
+export default { trilhasSonoras };
